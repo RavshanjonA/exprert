454b4e47b42cc8a7ac6e22c80c16c90ff3db4131
Ahmadjonov-98 <akhamdjonovr.98@gmail.com>
2021-09-25 09:58:23 +0500

    test for pages

--- basic/tests.py
+++ basic/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+class SimpleTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code,200)
 
-# Create your tests here.
+    def test_about_page_status_code(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_blog_page_status_code(self):
+        response = self.client.get('/blog/')
+        self.assertEqual(response.status_code,200)
+
+    def test_help_page_status_code(self):
+        response = self.client.get('/help/')
+        self.assertEqual(response.status_code, 200)
